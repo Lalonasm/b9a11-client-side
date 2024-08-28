c9bb202b2fbe94cf0cd0f6e32b3219ea4f954ef9
Lalon Sheikh <you@example.com>
2024-08-28 19:22:13 +0600

    one extra section added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,8 +3,12 @@ import Carousel from '../../components/banner/Carousel';
 import Blogs from './Blogs/Blogs';
 import RecentBlogs from './Blogs/RecentBlogs';
 import BookCategories from '../../components/BookCategories';
+import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
+    const books = useLoaderData();
+    console.log(books);
+
     return (
         <div>
             <Carousel></Carousel>
